21d66076af4a16af2958f71f00ea2b390a37166e
Mike Portillo <eugenio.alpha@gmail.com>
2020-05-04 14:50:47 -0600

    Commit inicial

---
+++ src/main/java/sv/com/mksoft/lacampesina/api/LaCampesinaApiApplication.java
@@ -0,0 +1,13 @@
+package sv.com.mksoft.lacampesina.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaCampesinaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaCampesinaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sv/com/mksoft/lacampesina/api/lacampesinaapi/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package sv.com.mksoft.lacampesina.api.lacampesinaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
